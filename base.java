3ba2089a58d9c0967f06e4bf965f5bbe5a268709
arjmoderator-ship-it <arjmoderator@gmail.com>
2025-10-17 23:24:35 +0700

    Add files via upload

---
+++ base.java
@@ -0,0 +1,7 @@
+/SmartSchool-Facility-Explorer
+│
+├── index.html          ← Halaman utama (beranda)
+├── uml.html            ← Halaman UML Module
+├── kontak.html         ← Halaman kontak
+├── style.css           ← Styling untuk semua halaman
+└── logo_cisat.png      ← Logo SMK CIBITUNG 1
